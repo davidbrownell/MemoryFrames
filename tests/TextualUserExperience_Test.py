892aaa92d8f4b5a61c0f72599707b0cae7efe4e4
David Brownell <db@davidbrownell.com>
2025-12-16 16:51:31 -0500

    ✨ [+feature] Added AppState

--- tests/TextualUserExperience_Test.py
+++
@@ -1,8 +0,0 @@
-from unittest.mock import Mock
-
-from MemoryFrames.TextualUserExperience import *
-
-
-# ----------------------------------------------------------------------
-def test_Execute():
-    Execute(Mock())
